536ee3244bcd08f58d15b060cc9406d9d06022a5
RodoBang <jorobanuelosga@ittepic.edu.mx>
2024-08-28 13:35:55 -0700

    Inicializa el codigo para Concatenacion de arrays

---
+++ 7. Concatenacion.js
@@ -0,0 +1,11 @@
+// Creamos dos arreglos de strings
+let frutas = ["manzana", "pera", "fresa"];
+let vegetales = ["lechuga", "coliflor", "zanahoria"];
+
+// Concatenamos los dos arreglos
+let alimentos = frutas.concat(vegetales);
+
+// Imprimimos el arreglo resultante
+console.log("Arreglo concatenado: "+alimentos);
+
+// La salida en consola es: Arreglo concatenado: manzana,pera,fresa,lechuga,coliflor,zanahoria
